56612caa8563e8383f42d183c1c1017a05a9494d
Umang <umangagarwal345@gmail.com>
2021-10-30 10:07:41 +0530

    Added code for adding users in room and setting up rtc connection

--- public/Assets/js/app.js
+++ public/Assets/js/app.js
@@ -1,7 +1,34 @@
+var AppProcess = (function(){
+
+    var iceConfiguration = {
+        iceServers : [
+            {
+                urls: "stun:stun.l.google.com:19302",   
+            },
+            {
+                urls: "stun:stun1.l.google.com:19302",   
+            },
+        ]
+    }
+
+    function setConnection(connId){
+        var connection = new RTCPeerConnection(iceConfiguration);
+    }
+
+    return {
+        setNewConnection : async function(connId){
+            await setConnection(connId);
+        }
+    }
+})
 var MyApp = (function(){
 
+    var user_id="";
+    var meeting_id="";
     function init(uid,mid)
     {
+         user_id=uid;
+         meeting_id = mid;
          event_process_for_signaling_server();
     }
 
@@ -10,8 +37,33 @@ var MyApp = (function(){
     {
          socket = io.connect();
          socket.on("connect",()=>{
-             alert("Socket connected to client side");
+             if(socket.connected)
+             {
+                  if(user_id!="" && meeting_id!="")
+                  {
+                       socket.connect("userconnect",{
+                            displayName : user_id,
+                            meetingid: meeting_id
+                       });
+                  }
+             }
          })
+
+         socket.on("inform_others_about_me",function(data){
+             addUser(data.other_user_id,data.connId);
+             AppProcess.setNewConnection(data.connId)
+         })
+    }
+
+    function addUser(other_user_id,connId)
+    {
+         var newDivId = $("#otherTemplate").clone();
+         newDivId = newDivId.attr("id".connId).addClass("other");
+         newDivId.find("h2").text(other_user_id);
+         newDivId.find("video").text("id","v_"+connId);
+         newDivId.find("audio").text("id","a_"+connId);
+         newDivId.show();
+         $("divUsers").append(newDivId);
     }
 
     return {
--- server.js
+++ server.js
@@ -10,7 +10,26 @@ const io = require("socket.io")(server,{
 });
 app.use(express.static(path.join(__dirname,"")));
 
+var userConnections = [];
 
 io.on("connection",(socket)=>{
     console.log("Socket id is ",socket.id);
+    socket.on("userconnect",(data)=>{
+        console.log("userconnect",data.displayName,data.meetingid);
+
+        var otherUsers = userConnections.filter((p)=> p.meeting_id == data.meetingid)
+
+        userConnections.push({
+            connectionId : socket.id,
+            user_id : data.displayName,
+            meeting_id : data.meetingid
+        });
+
+        otherUsers.forEach((v)=>{
+            socket.to(v.connectionId).emit("inform_others_about_me",{
+                other_user_id : data.displayName,
+                connId : socket.id 
+            })
+        })
+    });
 })
